cd220ce2f812378ee6ca3bc6d93bca091354c81e
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-05-23 13:59:40 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,27 +2,41 @@
   import { onMount } from 'svelte';
 
   let formFields = [];
-  let formData = {};
 
   async function fetchFormFields() {
     try {
-      const response = await fetch('https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3');
+      const response = await fetch('https://api.example.com/form-fields');
       formFields = await response.json();
     } catch (error) {
       console.error(error);
     }
   }
 
-  async function submitForm() {
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const formValues = {};
+
+    for (let [key, value] of formData.entries()) {
+      formValues[key] = value;
+    }
+
     try {
-      const response = await fetch('https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3', {
+      const response = await fetch('https://api.example.com/submit-form', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formValues)
       });
-      // Handle the response as needed
+
+      if (response.ok) {
+        // Handle successful form submission
+        console.log('Form submitted successfully!');
+      } else {
+        // Handle form submission error
+        console.error('Error submitting form');
+      }
     } catch (error) {
       console.error(error);
     }
@@ -33,23 +47,23 @@
 
 <main>
   {#if formFields.length > 0}
-    <form on:submit|preventDefault={submitForm}>
+    <form on:submit={handleSubmit}>
       {#each formFields as field}
         <label for={field.name}>{field.label}</label>
         {#if field.type === 'text'}
-          <input type="text" id={field.name} name={field.name} bind:value={formData[field.name]} />
-        {:else if field.type === 'checkbox'}
-          <input type="checkbox" id={field.name} name={field.name} bind:checked={formData[field.name]} />
-        {:else if field.type === 'select'}
-          <select id={field.name} name={field.name} bind:value={formData[field.name]}>
-            {#each field.options as option}
-              <option value={option}>{option}</option>
-            {/each}
-          </select>
+          <input type="text" id={field.name} name={field.name} />
+        {:else if field.type === 'email'}
+          <input type="email" id={field.name} name={field.name} />
+        {:else if field.type === 'number'}
+          <input type="number" id={field.name} name={field.name} />
+        {:else}
+          <!-- Handle other field types here -->
         {/if}
       {/each}
       <button type="submit">Submit</button>
     </form>
+  {:else}
+    <p>Loading form fields...</p>
   {/if}
 </main>
 
@@ -71,8 +85,7 @@
     margin-bottom: 0.5rem;
   }
 
-  input,
-  select {
+  input {
     padding: 0.5rem;
     margin-bottom: 1rem;
     width: 300px;
