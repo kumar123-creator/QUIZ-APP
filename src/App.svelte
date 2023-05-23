2c40d82b111149a73eecfb3bd32b6e5ffbc4278e
kumar123-creator <132423913+kumar123-creator@users.noreply.github.com>
2023-05-23 13:14:36 +0530

    Update App.svelte

--- src/App.svelte
+++ src/App.svelte
@@ -2,23 +2,51 @@
   import { onMount } from 'svelte';
 
   let formFields = [];
+  let formData = {};
 
-  onMount(async () => {
+  async function fetchFormFields() {
     try {
       const response = await fetch('https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3');
       formFields = await response.json();
     } catch (error) {
       console.error(error);
     }
-  });
+  }
+
+  async function submitForm() {
+    try {
+      const response = await fetch('https://api.recruitly.io/api/job?apiKey=TEST64518616D4CF145D4E20BD47169EA7229BA3', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+      // Handle the response as needed
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  onMount(fetchFormFields);
 </script>
 
 <main>
   {#if formFields.length > 0}
-    <form>
+    <form on:submit|preventDefault={submitForm}>
       {#each formFields as field}
         <label for={field.name}>{field.label}</label>
-        <input type={field.type} id={field.name} name={field.name} />
+        {#if field.type === 'text'}
+          <input type="text" id={field.name} name={field.name} bind:value={formData[field.name]} />
+        {:else if field.type === 'checkbox'}
+          <input type="checkbox" id={field.name} name={field.name} bind:checked={formData[field.name]} />
+        {:else if field.type === 'select'}
+          <select id={field.name} name={field.name} bind:value={formData[field.name]}>
+            {#each field.options as option}
+              <option value={option}>{option}</option>
+            {/each}
+          </select>
+        {/if}
       {/each}
       <button type="submit">Submit</button>
     </form>
@@ -45,7 +73,8 @@
     margin-bottom: 0.5rem;
   }
 
-  input {
+  input,
+  select {
     padding: 0.5rem;
     margin-bottom: 1rem;
     width: 300px;
